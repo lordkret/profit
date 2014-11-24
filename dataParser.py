123c7644521e45bde62dfbb9c4f55ba26a9937af
ehanlyn <hannah.lynch@ericsson.com>
2014-11-24 16:04:07 +0000

    Lucky Stars Data

---
+++ dataParser.py
@@ -0,0 +1,23 @@
+from bs4 import BeautifulSoup
+import urllib2                                       
+
+def get_page():
+    html = urllib2.urlopen("http://www.euro-millions.com/results-archive-2014") 
+    return html
+
+def get_stories(content):
+    soup = BeautifulSoup(content)
+    turnifier = 0
+    titles_html = ""
+
+    for div in soup.body.findAll("li", { "class":"lucky-star" }):
+        if (turnifier == 0): 
+            titles_html +=  div.get_text() + ","
+            turnifier = 1
+        else:
+            titles_html += div.get_text() + "\n"
+            turnifier = 0
+
+    return titles_html
+
+print get_stories(get_page())
