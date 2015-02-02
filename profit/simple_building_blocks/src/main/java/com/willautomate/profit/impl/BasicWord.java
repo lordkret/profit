775f113de9cc40d44692fc639edce0a14381e274
erafkos <rafal.kosmulski@ericsson.com>
2015-02-02 16:21:41 +0000

    toString

--- profit/simple_building_blocks/src/main/java/com/willautomate/profit/impl/BasicWord.java
+++ profit/simple_building_blocks/src/main/java/com/willautomate/profit/impl/BasicWord.java
@@ -1,8 +1,13 @@
 package com.willautomate.profit.impl;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Collections2;
+import com.google.common.collect.Lists;
 import com.willautomate.profit.api.Letter;
 import com.willautomate.profit.api.Word;
 
@@ -21,5 +26,17 @@ public class BasicWord implements Word {
 
 	public BasicWord(Letter<?>... letters){
 		this.letters = Arrays.asList(letters);
+	}
+	
+	public String toString(){
+		final DoubleBinarizer b = new DoubleBinarizer();
+		return String.format("a %s", Joiner.on("\n ").join(Collections2.transform(letters, new Function<Letter, String>() {
+
+			@Override
+			public String apply(Letter input) {
+				Letter<Double> val = input;
+				return Arrays.deepToString(b.debinarize(5, val.getRawData()));
+			}
+		})));
 	}
 }
--- profit/simple_building_blocks/src/test/java/com/willautomate/profit/ElmanWordDetectorTest.java
+++ profit/simple_building_blocks/src/test/java/com/willautomate/profit/ElmanWordDetectorTest.java
@@ -6,30 +6,34 @@ import com.willautomate.profit.api.WordFactory;
 import com.willautomate.profit.impl.BasicLetter;
 import com.willautomate.profit.impl.BasicWord;
 import com.willautomate.profit.impl.DoubleBinarizer;
+
 import org.apache.commons.lang3.ArrayUtils;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class ElmanWordDetectorTest {
 
     DoubleBinarizer binarizer = new DoubleBinarizer();
     @Test
-    public void trainingTest(){
+    public void trainingTest() throws IOException{
         ElmanWordDetector network = new ElmanWordDetector();
+        Word p = WordFactory.fromCsv(Paths.get("src/main/resources/fulldata.csv"), 1, 5, "M1","M2","M3","M4","M5",null,null);
         Letter<Double> l1 = new BasicLetter<Double>(binarizer.binarize(50,23, 29, 31, 39, 44));
         Letter<Double> l2 = new BasicLetter<Double>(binarizer.binarize(50,3, 7, 12, 13, 25));
         Letter<Double> l3 = new BasicLetter<Double>(binarizer.binarize(50, 2, 15, 28, 31, 37));
         Letter<Double> l4 = new BasicLetter<Double>(binarizer.binarize(50, 1,3,31,42,46));
         Word w = new BasicWord(l1,l2,l3,l4);
-        network.train(w);
-        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l1).getRawData(), 50, Double[].class))));
-        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l2).getRawData(), 50, Double[].class))));
-        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l3).getRawData(), 50, Double[].class))));
-        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l4).getRawData(), 50, Double[].class))));
+        System.out.println(p);
+//        network.train(p);
+//        System.out.println(p);
+//        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l1).getRawData(), 50, Double[].class))));
+//        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l2).getRawData(), 50, Double[].class))));
+//        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l3).getRawData(), 50, Double[].class))));
+//        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(l4).getRawData(), 50, Double[].class))));
         
-        Letter<Double> veryRandom = new BasicLetter<Double>(binarizer.binarize(50, 4,6,28,31,46));
-        System.out.println(Arrays.deepToString(binarizer.debinarize(5,Arrays.copyOf(network.predict(veryRandom).getRawData(), 50, Double[].class))));
-        
+       
     }
 }
