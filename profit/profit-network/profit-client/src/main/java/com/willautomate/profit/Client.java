6cc95a1fe2188a931e7e1d16c59a4d690a07611d
erafkos <rafal.kosmulski@ericsson.com>
2015-04-17 10:36:39 +0100

    get mains

--- profit/profit-network/profit-client/src/main/java/com/willautomate/profit/Client.java
+++ profit/profit-network/profit-client/src/main/java/com/willautomate/profit/Client.java
@@ -14,22 +14,22 @@ public class Client {
 		System.out.println("Starting");
 		Beemo.register();
 		for (int i = 0; i < 1000; i++) {
-			wordSerice.execute(new LuckyWalker(4500+i,NetworkPattern.Elmann,2,50));
-			wordSerice.execute(new LuckyWalker(5500+i,NetworkPattern.Elmann,50,60));
-			wordSerice.execute(new LuckyWalker(6500+i,NetworkPattern.Elmann,60,70));
-			wordSerice.execute(new LuckyWalker(7500+i,NetworkPattern.Elmann,70,80));
-			wordSerice.execute(new LuckyWalker(8500+i,NetworkPattern.Elmann,80,90));
-			wordSerice.execute(new LuckyWalker(9500+i,NetworkPattern.Elmann,90,100));
-			wordSerice.execute(new LuckyWalker(10000+i,NetworkPattern.Elmann,100,127));
+//			wordSerice.execute(new LuckyWalker(4500+i,NetworkPattern.Elmann,2,50));
+//			wordSerice.execute(new LuckyWalker(5500+i,NetworkPattern.Elmann,50,60));
+//			wordSerice.execute(new LuckyWalker(6500+i,NetworkPattern.Elmann,60,70));
+//			wordSerice.execute(new LuckyWalker(7500+i,NetworkPattern.Elmann,70,80));
+//			wordSerice.execute(new LuckyWalker(8500+i,NetworkPattern.Elmann,80,90));
+//			wordSerice.execute(new LuckyWalker(9500+i,NetworkPattern.Elmann,90,100));
+			wordSerice.execute(new LuckyWalker(10000+i,NetworkPattern.Elmann,2,127));
 //			wordSerice.execute(new LuckyWalker(i,NetworkPattern.ElmannStep));
-//			wordSerice.execute(new GrowingWordWalker(100+i,2,NetworkPattern.Elmann));
-//			wordSerice.execute(new GrowingWordWalker(i,1,NetworkPattern.ElmannStep));
-//			wordSerice.execute(new GrowingWordWalker(1000+i,0,NetworkPattern.ElmannStep));
+			wordSerice.execute(new GrowingWordWalker(100+i,2,NetworkPattern.Elmann));
+			wordSerice.execute(new GrowingWordWalker(i,1,NetworkPattern.ElmannStep));
+			wordSerice.execute(new GrowingWordWalker(1000+i,0,NetworkPattern.ElmannStep));
 //			wordSerice.execute(new GrowingWordWalker(i,3,NetworkPattern.ElmannStep));
 //            wordSerice.execute(new GrowingWordWalker(i,3));
 //			wordSerice.execute(new GrowingWordWalker(i,1));
         }
-		wordSerice.awaitTermination(120, TimeUnit.HOURS);
+		wordSerice.awaitTermination(7, TimeUnit.HOURS);
 		
 		System.out.println("Ready to stop");
 		Beemo.unregister();
