a602725c3b30fa3288a336fe950df9e387ee9e40
erafkos <rafal.kosmulski@ericsson.com>
2015-04-18 19:52:15 +0100

    disabling matrace detection by default

--- profit/profit-network/profit-client/src/main/java/com/willautomate/profit/Client.java
+++ profit/profit-network/profit-client/src/main/java/com/willautomate/profit/Client.java
@@ -14,12 +14,12 @@ public class Client {
 		System.out.println("Starting");
 		Beemo.register();
 		for (int i = 0; i < 1000; i++) {
-//			wordSerice.execute(new LuckyWalker(4500+i,NetworkPattern.Elmann,2,50));
-//			wordSerice.execute(new LuckyWalker(5500+i,NetworkPattern.Elmann,50,60));
-//			wordSerice.execute(new LuckyWalker(6500+i,NetworkPattern.Elmann,60,70));
-//			wordSerice.execute(new LuckyWalker(7500+i,NetworkPattern.Elmann,70,80));
-//			wordSerice.execute(new LuckyWalker(8500+i,NetworkPattern.Elmann,80,90));
-//			wordSerice.execute(new LuckyWalker(9500+i,NetworkPattern.Elmann,90,100));
+			wordSerice.execute(new LuckyWalker(4500+i,NetworkPattern.Elmann,2,50));
+			wordSerice.execute(new LuckyWalker(5500+i,NetworkPattern.Elmann,50,60));
+			wordSerice.execute(new LuckyWalker(6500+i,NetworkPattern.Elmann,60,70));
+			wordSerice.execute(new LuckyWalker(7500+i,NetworkPattern.Elmann,70,80));
+			wordSerice.execute(new LuckyWalker(8500+i,NetworkPattern.Elmann,80,90));
+			wordSerice.execute(new LuckyWalker(9500+i,NetworkPattern.Elmann,90,100));
 			wordSerice.execute(new LuckyWalker(10000+i,NetworkPattern.Elmann,2,127));
 //			wordSerice.execute(new LuckyWalker(i,NetworkPattern.ElmannStep));
 			wordSerice.execute(new GrowingWordWalker(100+i,2,NetworkPattern.Elmann));
