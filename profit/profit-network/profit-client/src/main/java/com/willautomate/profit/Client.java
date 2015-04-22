78f06c8c63f155470267dfd642567b4097673d32
erafkos <rafal.kosmulski@ericsson.com>
2015-04-22 16:22:28 +0100

    sampling on small wordsize

--- profit/profit-network/profit-client/src/main/java/com/willautomate/profit/Client.java
+++ profit/profit-network/profit-client/src/main/java/com/willautomate/profit/Client.java
@@ -14,7 +14,12 @@ public class Client {
 		System.out.println("Starting");
 		Beemo.register();
 		for (int i = 0; i < 1000; i++) {
-			wordSerice.execute(new LuckyWalker(4500+i,NetworkPattern.Elmann,2,50));
+			wordSerice.execute(new LuckyWalker(200+i,NetworkPattern.Elmann,2,5));
+			wordSerice.execute(new LuckyWalker(250+i,NetworkPattern.Elmann,5,10));
+			wordSerice.execute(new LuckyWalker(300+i,NetworkPattern.Elmann,10,15));
+			wordSerice.execute(new LuckyWalker(350+i,NetworkPattern.Elmann,15,20));
+			wordSerice.execute(new LuckyWalker(400+i,NetworkPattern.Elmann,20,25));
+			wordSerice.execute(new LuckyWalker(450+i,NetworkPattern.Elmann,25,50));
 			wordSerice.execute(new LuckyWalker(5500+i,NetworkPattern.Elmann,50,60));
 			wordSerice.execute(new LuckyWalker(6500+i,NetworkPattern.Elmann,60,70));
 			wordSerice.execute(new LuckyWalker(7500+i,NetworkPattern.Elmann,70,80));
